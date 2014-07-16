190a2fe76600d251e72188009fd27a7c1eb67758
Simon Kiener <jugglinghobo@gmail.com>
2014-07-16 14:31:32 +0200

    added nav helpers for dynamic active class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def nav_bar
+    content_tag(:div, :class => "navbar-nav navbar-inverse navbar-fixed-top", :role => "navigation") do
+      content_tag(:div, :class => "container") do
+        content_tag(:ul, :class => "nav navbar-nav") do
+          yield
+        end
+      end
+    end
+  end
+
+  def nav_link(text, path)
+    options = current_page?(path) ? { :class => "active" } : {}
+    content_tag(:li, options) do
+      link_to text, path
+    end
+  end
 end
