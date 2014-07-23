137a9455f5c12c4c1fc4e314c2152288bc7dcfcc
Simon Kiener <jugglinghobo@gmail.com>
2014-07-23 11:16:01 +0200

    beers can be created on beers index page

--- app/assets/javascripts/beers.js
+++ app/assets/javascripts/beers.js
@@ -1,10 +1,11 @@
-$(document).ready(function() {
+$(document).on("ready page:load", function() {
   Beers.initialize();
 });
 
 var Beers = function() {
   var handleBeerUpdate = function() {
     $("#new_beer").on("ajax:success", function(xhr, data, status) {
+      debugger;
       $("#beers_list").html(data);
     });
   }
